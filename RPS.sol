f5323f2225dcdb06dc47606a50039722385ba887
icecreamna <kittabeth2547@gmail.com>
2025-03-03 19:27:29 +0700

    tse2

--- RPS.sol
+++ RPS.sol
@@ -13,6 +13,13 @@ contract RPS {
     uint public numInput = 0;
 
     function addPlayer() public payable {
+        require(
+            msg.sender == 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 || 
+            msg.sender == 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 ||
+            msg.sender == 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db || 
+            msg.sender == 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,
+            "You're not allowed to join this game"
+        );
         require(numPlayer < 2);
         if (numPlayer > 0) {
             require(msg.sender != players[0]);
@@ -27,7 +34,7 @@ contract RPS {
     function input(uint choice) public  {
         require(numPlayer == 2);
         require(player_not_played[msg.sender]);
-        require(choice == 0 || choice == 1 || choice == 2);
+        require(choice == 0 || choice == 1 || choice == 2 || choice == 3 || choice == 4);
         player_choice[msg.sender] = choice;
         player_not_played[msg.sender] = false;
         numInput++;
@@ -41,11 +48,11 @@ contract RPS {
         uint p1Choice = player_choice[players[1]];
         address payable account0 = payable(players[0]);
         address payable account1 = payable(players[1]);
-        if ((p0Choice + 1) % 3 == p1Choice) {
+        if ((p0Choice + 1) % 5 == p1Choice || (p0Choice + 3 ) % 5 == p1Choice) {
             // to pay player[1]
             account1.transfer(reward);
         }
-        else if ((p1Choice + 1) % 3 == p0Choice) {
+        else if ((p1Choice + 1) % 3 == p0Choice || (p1Choice + 3) % 5 == p0Choice) {
             // to pay player[0]
             account0.transfer(reward);    
         }
@@ -54,5 +61,9 @@ contract RPS {
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
         }
+        numPlayer = 0;
+        reward = 0;
+        numInput = 0;
+        delete players ;
     }
 }
